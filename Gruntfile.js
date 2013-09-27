dafbc3cf036acd23d0dc4e5cbebb88cfed807048
Toby Mackenzie <public@tobymackenzie.com>
2013-09-27 01:29:03 -0400

    move building to grunt.  now have a plain requirejs build.  moved builds to 'dist' folder.  removed builds from repo.

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,9 +20,46 @@ module.exports = function(__grunt){
 		,qunit: {
 			all: ['test/**/*.html']
 		}
+		,requirejs: {
+			almond: {
+				options: {
+					baseUrl: 'build'
+					,include: ['../vendor/almond/almond.js', 'requirements']
+					,out: 'dist/tmclasses.almond.js'
+					,paths: {
+						tmclasses: '../src'
+					}
+					,wrap: {
+						startFile: 'build/wrap.start.js'
+						,endFile: 'build/wrap.end.js'
+					}
+				}
+			}
+			,require: {
+				options: {
+					baseUrl: 'build'
+					,optimize: 'none'
+					// ,include: ['tmclasses/BaseClass']
+					,name: 'tmclasses/BaseClass'
+					,out: 'dist/tmclasses.require.js'
+					,paths: {
+						tmclasses: '../src'
+					}
+				}
+			}
+		}
 	});
 
 	//==tasks
+	__grunt.registerTask('build', [
+		'jshint'
+		,'requirejs:require'
+	]);
+	__grunt.registerTask('build:all', [
+		'jshint'
+		,'requirejs:almond'
+		,'requirejs:require'
+	]);
 	__grunt.registerTask('default', [
 		'jshint'
 		,'qunit'
